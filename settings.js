65b4088149ae92ccc925cc714b41c6f4b6c7e9e3
Lokuachikingboyoffical <109135065+Lokuachikingboyoffical@users.noreply.github.com>
2022-07-18 22:36:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['94786911147']
+global.premium = ['94786911147']
+global.ownernomer = '94786911147'
 global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
+global.botname = 'BIXBY-MD USER Achi Queen BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
